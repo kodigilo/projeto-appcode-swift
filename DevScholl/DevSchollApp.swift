814990eaf534cf6631e3613eeaba742b6b3a6020
Gabriel Alencar <gabriel@alencar.site>
2020-10-12 01:37:46 -0300

    Start

---
+++ DevScholl/DevSchollApp.swift
@@ -0,0 +1,18 @@
+//
+//  DevSchollApp.swift
+//  DevScholl
+//
+//  Created by Gabriel Alencar on 12/10/20.
+//
+//
+
+import SwiftUI
+
+@main
+struct DevSchollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
